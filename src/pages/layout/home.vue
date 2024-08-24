00fbfc9f6590a74f73d06e4a55e9ad3a2b4a3309
erkelost <1256029807@qq.com>
2024-08-24 18:38:07 +0800

    chore: update code

--- src/pages/layout/home.vue
+++ src/pages/layout/home.vue
@@ -6,14 +6,29 @@ import { listen } from "@tauri-apps/api/event";
 // const axle = createAxle();
 
 // const res = await axle.get("/wallhaven/w/3lv8j6");
-fetch("https://heaven-walls-api.vercel.app/api/wallhaven/w/3lv8j6")
-  .then((res) => res.json())
-  .then(console.log);
-async function changePaper() {
+const imageData = ref([]);
+const topQuery = reactive({
+  page: 1,
+  toprange: "3d"
+});
+async function query() {
+  topQuery.page = topQuery.page + 1;
+  const res = await fetch(
+    `https://heaven-walls-api.vercel.app/api/wallhaven/topwalls?page=${topQuery.page}&toprange=${topQuery.toprange}`
+  );
+  const data = await res.json();
+  console.log(data);
+  imageData.value = data.data
+}
+query();
+async function changePaper(item) {
   await invoke("download_and_set_wallpaper", {
-    url: "https://w.wallhaven.cc/full/yx/wallhaven-yx6e9l.jpg",
-    fileName: "pic",
+    url: item.path,
+    fileName: "wallhaven-" + item.id
   });
+}
+async function queryPaper() {
+  query();
 }
 </script>
 
@@ -22,8 +37,22 @@ async function changePaper() {
     <app-page-header title="Home" description="This is the home page" />
 
     <app-page-content>
+      <var-button @click="queryPaper">换下一页</var-button>
       <var-button @click="changePaper">修改壁纸</var-button>
-      <img src="../../assets/images/test.jpg" style="width: 100%" alt="" />
+      <div class="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-20px">
+        <var-card
+          v-for="i in imageData"
+          :key="i"
+          @click="changePaper(i)"
+        >
+          <var-image
+            lazy
+            :loading="i.path"
+            :error="i.path"
+            :src="i.path"
+          />
+        </var-card>
+      </div>
     </app-page-content>
   </app-page>
 </template>
