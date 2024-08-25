cc63441ad95828f82b9fa4b6c5bbe1bb62f67748
erkelost <1256029807@qq.com>
2024-08-25 19:15:41 +0800

    chore: update home page

---
+++ src/components/swiper/index.tsx
@@ -0,0 +1,31 @@
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/effect-cards';
+
+
+// import required modules
+import { EffectCards } from 'swiper/modules';
+
+export default function App({data}) {
+  return (
+    <>
+      <Swiper
+        effect={'cards'}
+        grabCursor={true}
+        modules={[EffectCards]}
+        className="mySwiper"
+      >
+        {data?.map((item) => {
+          return (
+            <SwiperSlide key={item.id}>
+              <img className='w-full h-full object-cover' src={item.path} alt="wallpaper" onClick={() => console.log(123123)} />
+            </SwiperSlide>
+          )
+        })}
+      </Swiper>
+    </>
+  );
+}
