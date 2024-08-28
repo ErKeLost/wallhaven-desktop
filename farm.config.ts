37d75b2c58ee9719b43e1f76319d45a2cb796641
erkelost <1256029807@qq.com>
2024-08-29 00:04:16 +0800

    chore: update 样式

--- farm.config.ts
+++ farm.config.ts
@@ -10,6 +10,9 @@ export default defineConfig({
       alias: {
         "@": path.resolve(__dirname, "src"),
       }
+    },
+    output: {
+      targetEnv: 'browser-esnext'
     }
   },
   server: {
