814a7e284f95aa20769e729de2bd9349f32fc141
阿菜 Cai <jimmyrss1102@gmail.com>
2024-10-10 03:23:45 +0800

    style: 优化搜索栏

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -3,7 +3,7 @@ import * as React from "react"
 import { cn } from "@/lib/utils"
 
 export interface InputProps
-  extends React.InputHTMLAttributes<HTMLInputElement> {}
+  extends React.InputHTMLAttributes<HTMLInputElement> { }
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
   ({ className, type, ...props }, ref) => {
@@ -11,7 +11,14 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
       <input
         type={type}
         className={cn(
-          "flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
+          "flex h-10 w-full rounded-md",
+          "border border-input dark:border-white/20 focus:border-black/20 dark:focus:border-white/20",
+          "bg-background px-3 py-2",
+          "text-sm placeholder:text-muted-foreground dark:placeholder:text-white/60",
+          "ring-offset-background focus:outline-none focus:ring-0 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2",
+          "file:border-0 file:bg-transparent file:text-sm file:font-medium",
+          "disabled:cursor-not-allowed disabled:opacity-50",
+          "pl-10 pr-10 min-w-[220px]",
           className
         )}
         ref={ref}
